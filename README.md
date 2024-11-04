75805721dbec27ff453bb8228d82fdff20c66dcf
adarshkumarhembram <hembramadarshkumar@gmail.com>
2024-11-04 07:45:50 +0530

    Added react hot toast

--- README.md
+++ README.md
@@ -62,3 +62,5 @@
 ```
     npm install @reduxjs/toolkit react-redux react-router-dom react-icons react-chartjs-2 chart.js daisyui react-hot-toast @tailwindcss/line-clamp axios
 ```
+
+### Configure auto import sort esline
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './index.css';
+
+import App from './App.jsx';
+
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 
 createRoot(document.getElementById('root')).render(
- 
-    <App />
-  
+    <BrowserRouter>
+      <App />
+      <Toaster/>
+    </BrowserRouter>
 )
